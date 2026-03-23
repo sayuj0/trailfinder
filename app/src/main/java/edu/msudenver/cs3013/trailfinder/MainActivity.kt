b4da17aabd0af9da7e180a8673061ed719140a49
Sayuj Shrestha <sayuj21@gmail.com>
2026-03-23 15:05:48 -0600

    Wire drawer navigation in MainActivity

--- app/src/main/java/edu/msudenver/cs3013/trailfinder/MainActivity.kt
+++ app/src/main/java/edu/msudenver/cs3013/trailfinder/MainActivity.kt
@@ -2,10 +2,47 @@ package edu.msudenver.cs3013.trailfinder
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.homeFragment,
+                R.id.trailListFragment,
+                R.id.favoritesFragment,
+                R.id.aboutFragment
+            ),
+            drawerLayout = findViewById(R.id.drawer_layout)
+        )
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) ||
+                super.onSupportNavigateUp()
     }
 }
