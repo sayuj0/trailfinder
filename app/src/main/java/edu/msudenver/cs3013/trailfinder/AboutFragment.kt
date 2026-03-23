cb1efe74e1f909631db69c6743c52b0cb845f130
Sayuj Shrestha <sayuj21@gmail.com>
2026-03-23 15:04:27 -0600

    Add stub favorites and about fragments

---
+++ app/src/main/java/edu/msudenver/cs3013/trailfinder/AboutFragment.kt
@@ -0,0 +1,5 @@
+package edu.msudenver.cs3013.trailfinder
+
+import androidx.fragment.app.Fragment
+
+class AboutFragment : Fragment(R.layout.fragment_about)
---
+++ app/src/main/java/edu/msudenver/cs3013/trailfinder/FavoritesFragment.kt
@@ -0,0 +1,5 @@
+package edu.msudenver.cs3013.trailfinder
+
+import androidx.fragment.app.Fragment
+
+class FavoritesFragment : Fragment(R.layout.fragment_favorites)
