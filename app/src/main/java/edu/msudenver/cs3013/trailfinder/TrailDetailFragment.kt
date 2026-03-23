dcd1f84e3def3d380b80e3e1fcb3c5cd51f528e0
Sayuj Shrestha <sayuj21@gmail.com>
2026-03-23 14:47:56 -0600

    Use Safe Args in trail detail fragment

--- app/src/main/java/edu/msudenver/cs3013/trailfinder/TrailDetailFragment.kt
+++ app/src/main/java/edu/msudenver/cs3013/trailfinder/TrailDetailFragment.kt
@@ -5,21 +5,23 @@ import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
 
 class TrailDetailFragment : Fragment(R.layout.fragment_trail_detail) {
 
+    private val args: TrailDetailFragmentArgs by navArgs()
     private val viewModel: TrailViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val trail = viewModel.getTrailById(0)
+        val trail = viewModel.getTrailById(args.trailId)
 
         trail?.let {
             view.findViewById<TextView>(R.id.detailName).text = it.name
             view.findViewById<TextView>(R.id.detailLocation).text = it.location
-            view.findViewById<TextView>(R.id.detailDistance).text = it.distance
-            view.findViewById<TextView>(R.id.detailDifficulty).text = it.difficulty
+            view.findViewById<TextView>(R.id.detailDistance).text = "Distance: ${it.distance}"
+            view.findViewById<TextView>(R.id.detailDifficulty).text = "Difficulty: ${it.difficulty}"
             view.findViewById<TextView>(R.id.detailDescription).text = it.description
         }
     }
